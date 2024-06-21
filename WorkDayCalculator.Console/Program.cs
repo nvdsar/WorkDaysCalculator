31f65b249b2e5545c17a019d5e939d777a299081
Navid Sargheiny <nvdsar10133@gmail.com>
2024-06-21 08:29:53 +0330

    remove unnecessary extensions and put writeline in console application

--- WorkDayCalculator.Console/Program.cs
+++ WorkDayCalculator.Console/Program.cs
@@ -3,3 +3,5 @@
 var from = new DateTime(2024, 06, 01);
 var to = DateTime.Now;
 var count = new Calculator().GetWorkDaysCount(from, to, HolidayRegion.SolarHijri, HolidayRegion.LunarHijri);
+Console.WriteLine(count);
+Console.ReadKey();  
